56baba8532d85bcddd7def772cefcfe702f1b2f7
akky6srivastava <akkyjaicar@gmail.com>
2020-11-22 01:29:05 +0530

    initial commit

---
+++ plan.md
@@ -0,0 +1,9 @@
+# Plan of Action
+
+-Initialize our NodeJS project
+-Initialize our first View
+-Room Creation(Genration of Room ID)
+-Add the ability to view our own video
+-Stream Video by other users
+-Mute Button
+-Stop Button
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+//const cors = require('cors');
+
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+const { v4: uuidv4 } = require('uuid')
+
+
+//app.use(cors());
+app.use('/peerjs', peerServer);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) =>{
+    res.render('room', { roomId : req.params.room })
+    
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
+})
+
+server.listen(process.env.PORT || 3000)

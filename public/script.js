a197d6d85b58d4804f500bd120271b6448217d63
akky6srivastava <akkyjaicar@gmail.com>
2020-11-25 08:48:38 +0530

    initial commit

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ myVideo.muted = true;
 const peer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '443',
+    port: '3000',
     pingInterval: '6000'
 });
 
@@ -29,6 +29,14 @@ navigator.mediaDevices.getUserMedia({
         })
     })
 
+    $('.upload').click(()=>{
+      socket.emit("file-submit")
+  })
+    socket.on('file-shared',src=>{
+      console.log(src)
+      shareTheFile(src)
+  })
+
     socket.on('user-connected', (userId) => {
         connectToNewUser(userId, stream);
     })
@@ -48,6 +56,10 @@ navigator.mediaDevices.getUserMedia({
         socket.emit('join-room', ROOM_ID, id);
     })
 
+    const shareTheFile = (src) =>{
+      let html = `<li class="chat__convo"><h5>${src}</h5><form action="http://localhost:3000/download${src}" method="GET"><button class="btn btn-warning" type="submit">Download File</button></form></li>`;
+      $('ul').append(html) 
+  }
 
 const connectToNewUser = (userId,stream) => {
     console.log(userId)
@@ -70,12 +82,6 @@ const addVideoStream = (video, stream) => {
     })
     videoGrid.append(video);
 }
-
-const scrollToBottom = () => {
-    var d = $('.main__chat_window');
-    d.scrollTop(d.prop("scrollHeight"));
-  }
-  
   
   const muteUnmute = () => {
     const enabled = myVideoStream.getAudioTracks()[0].enabled;
@@ -131,3 +137,172 @@ const scrollToBottom = () => {
     `
     document.querySelector('.main__video_button').innerHTML = html;
   }
+
+  //Torch
+  $('.main__torch').click(()=>{
+    // console.log(userId)
+    socket.emit('torch')
+})
+socket.on('torched-on',(user) =>{
+    if(1){
+    $('.main__videos').css('background','whitesmoke')
+    $('.main__left').prepend(`<h5 class="torch_user"> Someone Torched on</h5>`);
+    setTimeout(()=>{
+    $('.main__videos').css('background','black')
+    $('.torch_user').remove()
+    },5000)
+  }
+})
+
+//chats
+let text = $("input");
+  // when press enter send message
+  $('html').keydown(function(e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit('message', text.val());
+      text.val('')
+    }
+});
+  socket.on("createMessage", (userId, message) => {
+    $("ul").append(`<li class="message"><b>${userId}</b><br/>${message}</li>`);
+    scrollToBottom()
+})
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+//Recording
+const start = document.getElementById("start");
+const Stop = document.getElementById("stop");
+const video = document.querySelector("video");
+let recorder, stream;
+console.log(myVideoStream)
+async function startRecording() {
+// const audio = myVideoStream.getAudioTracks()
+  
+stream = await navigator.mediaDevices.getDisplayMedia({
+    video: { mediaSource: "screen" },
+    audio:true
+  });
+  recorder = new MediaRecorder(stream);
+
+  const chunks = [];
+  recorder.ondataavailable = e => chunks.push(e.data);
+  recorder.onstop = e => {
+    const completeBlob = new Blob(chunks, { type: chunks[0].type });
+    video.src = URL.createObjectURL(completeBlob);
+  };
+
+  recorder.start();
+}
+
+start.addEventListener("click", () => {
+
+$('#start').css('visibility','hidden')
+$('#stop').css('visibility','visible')
+
+  startRecording();
+
+});
+Stop.addEventListener("click", () => {
+
+$('#start').css('visibility','visible')
+$('#stop').css('visibility','hidden')
+$('.record__video').css('visibility','visible')
+//   recorder.stop();
+  stream.getVideoTracks()[0].stop();
+  stream.getAudioTracks()[0].stop();
+});
+
+//screen sharing
+socket.on('shareTheScreen',(userId,src)=>{
+  console.log(src)
+  console.log(userId)
+})
+
+var startScreen = document.getElementById("startScreen")
+var startVideo = document.getElementById("startVideo")
+var stopScreen = document.getElementById("stopScreen")
+var videoScreen = document.createElement("video")
+
+var displayMediaOption = {
+videoScreen:{
+  cursor:'always'
+},
+audio:false
+}
+
+
+startScreen.addEventListener("click", function(e){
+startscreen();
+},false)
+//let Firstusername = userId
+async function startscreen(){
+
+try{
+  socket.on('share-screen', (userId) => {
+  console.log(userId)
+
+  });
+  if(myVideoStream===stream){
+  let stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOption);
+  stream.inactive = false
+  myVideoStream = stream; 
+  console.log(myVideoStream)
+  oneTimeShare = false   
+  //let username = +'(Screening)'
+  socket.emit('join-room', ROOM_ID, id) 
+      myVideo.srcObject = stream
+      myVideo.removeAttribute("class")
+      const html = `<i class=" fas fa-desktop"></i>
+                      <span style="font-size:18px">Stop</span>`;
+      document.querySelector("#startScreen").innerHTML = html;
+  }
+  else{
+  //let username = userId+'(Screening)'
+  socket.emit('screen-share-cancel',null) 
+      myVideoStream = stream;
+      myVideo.setAttribute("class","VideoStreams")
+      socket.emit('join-room', ROOM_ID, id) 
+      myVideo.srcObject = stream;
+        const html = `<i class=" fas fa-desktop"></i>
+                      <span style="font-size:18px">Share Screen</span>`;
+      document.querySelector("#startScreen").innerHTML = html;
+  }        
+}
+
+catch(err){
+  console.error("Error" + err)
+}
+}
+
+function stopscreen(e){
+let tracks = videoScreen.srcObject.getTracks()
+
+tracks.forEach(track => track.stop())
+
+videoScreen.srcObject = null;
+}
+
+async function startvideo(){
+try{
+  videoScreen.srcObject = await navigator.mediaDevices.getUserMedia(displayMediaOption);
+}
+
+catch(err){
+  console.error("Error" + err)
+}
+}
+
+//swictching b/w chat and user
+$('.main__controls__ppl').click(()=>{
+  $('.chat').css("display","none");
+  $('.userslist').css("display", "block")
+})
+
+$('.main__controls__chat').click(()=>{
+  $('.userslist').css("display","none")
+  $('.chat').css("display", "block");
+})
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
+const formidable = require('formidable')
+const path = require('path');
+let fs = require('fs')
+const cookieParser = require('cookie-parser')
+
 //const cors = require('cors');
 
 
@@ -12,25 +17,111 @@ const peerServer = ExpressPeerServer(server, {
 
 const { v4: uuidv4 } = require('uuid')
 
-
+let dataFile ;
+let error = ''
+let usersList = []
 //app.use(cors());
+app.use(cookieParser())
 app.use('/peerjs', peerServer);
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.redirect(`/${uuidv4()}`)
+app.get('/', (req, res) =>{
+    res.redirect(`${uuidv4()}`)
 })
 
-app.get('/:room', (req, res) =>{
-    res.render('room', { roomId : req.params.room })
-    
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room})
 })
+
+app.get('/atte',(req,res)=>{
+    res.download(__dirname +`/attendance.txt`);   
+ })
+
+app.get('/download/:src',(req,res)=>{
+    let source=req.params.src
+    console.log(source)
+   res.download(__dirname + '/' +source);   
+})
+  function get(file){
+      console.log(file)
+      if(file===''){
+          return dataFile
+      }
+      else{
+        dataFile = file 
+        return true
+      }
+  }
+app.post('/api/file', function(req, res) {
+    var form = new formidable.IncomingForm();
+      // specify that we want to allow the user to upload multiple files in a single request
+      form.multiples = true;
+      // store all uploads in the /uploads directory
+      form.uploadDir = path.basename(path.dirname('/public'))
+      // every time a file has been uploaded successfully,
+      // rename it to it's orignal name
+      form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name), function(err){
+            if (err) throw err;
+            //console.log('renamed complete: '+file.name);
+            const file_path = '/'+file.name
+            file = file_path
+            get(file)
+            console.log(file)
+        });
+      });
+      // log any errors that occur
+      form.on('error', function(err) {
+          console.log('An error has occured: \n' + err);
+      });
+      // once all the files have been uploaded, send a response to the client
+      form.on('end', function() {
+           res.statusMessage = "Process cashabck initiated";
+           res.statusCode = 200;
+         res.status(204).send();
+        
+      });
+      // parse the incoming request containing the form data
+      form.parse(req);
+  })
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId);
+
+        socket.on('torch',userId=>{
+            io.to(roomId).emit('torched-on',userId)
+        })
+
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', userId,message)
+        })
+
+        socket.on('file-submit',async()=>{
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            let srcfile = get('')
+            console.log(srcfile)
+            if(1){
+            io.to(roomId).emit('file-shared',srcfile)
+
+            }
+          })
+
+          socket.on('screen-shared',(src)=>{
+            //  const _srcObject = ss.createStream();
+            console.log(src)
+            console.log('From screen share', userId)
+            io.to(roomId).emit('share-screen',src);
+        })
+
+        socket.on('screen-share-cancel', () => {
+           //refresh()
+          // io.to(roomId).emit('refresh',roomUsers)
+           io.to(roomId).emit('screen-share-remove', null)
+       })
 
         socket.on('disconnect', () => {
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
